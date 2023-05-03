b2a5629288069bad39b3a510c68424d28ab24ea1
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2023-05-03 23:34:07 +0300

    Добавьте файлы проекта.

---
+++ HumanInheritance.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Human {
+public:
+    Human() {
+        name = "Human";
+        cout << "Human()" << endl;
+    }
+    Human(const string& name):name(name){
+        cout << "Human(string)" << endl;
+    }
+    void work() {
+        cout << "Human" << name << " do work" << endl;
+    }
+    ~ Human() {
+        cout << " ~ Human()" << endl;
+    }
+public:
+    string name;
+
+ };
+
+class Doctor : public Human {
+public:
+    Doctor() {
+        cout << "Doctor()" << endl;
+    }
+    Doctor(const string& name, const int kol):Human(name), kol_pacientov(kol){
+        cout << "Doctor(string, int)" << endl;
+    }
+    ~Doctor() {
+        cout << " ~ Doctor()" << endl;
+    }
+public:
+    int kol_pacientov = 0;
+
+};
+
+int main()
+{
+    Human h("Jack");
+    h. work();
+
+
+    
+}
